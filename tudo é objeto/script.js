749fe3206dff7e7de55399b83b7594695fc40fd6
WellingtonMarques96 <well1506marques@gmail.com>
2023-01-03 17:53:13 -0300

    objeto 2

---
+++ tudo é objeto/script.js
@@ -0,0 +1,18 @@
+// nomeie 3 propriedades ou métodos de strings
+var cidade = "betim";
+
+length;
+fixed;
+toUpperCase;
+
+// nomeie 5 propriedades ou métodos de elementos do DOM
+var btn = document.querySelector("a");
+
+btn.addEventListener;
+btn.appendChild;
+btn.animate;
+btn.innerHTML;
+btn.click;
+
+// busque na web um objeto (método) capaz de interagir com o clipboard,
+// clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V
