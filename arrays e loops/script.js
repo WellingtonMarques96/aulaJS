d498af79fc0eed1ad2ee8479f64d8e29f0bb072a
WellingtonMarques96 <well1506marques@gmail.com>
2023-01-03 19:15:41 -0300

    arrays e loops

---
+++ arrays e loops/script.js
@@ -0,0 +1,15 @@
+// Crie uma array com os anos que o Brasil ganhou a copa
+// 1958, 1962, 1970, 1994, 2002
+var brasilCampeao = ["1958", "1962", "1970", "1994", "2002"];
+
+// Interaja com a array utilizando um loop, para mostrar
+// no console a seguinte mensagem, `O brasil ganhou a copa de ${ano}`
+for (var i = 0; i < brasilCampeao.length; i++) {
+  console.log(`O brasil ganhou a copa de ${brasilCampeao[i]}`);
+}
+
+// Interaja com um loop nas frutas abaixo e pare ao chegar em Pera
+var frutas = ["Banana", "Maçã", "Pera", "Uva", "Melância"];
+
+// Coloque a última fruta da array acima em uma variável,
+// sem remover a mesma da array.
